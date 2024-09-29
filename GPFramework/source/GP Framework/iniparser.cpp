dc133d9b9e9228a576330cfb912b39f393fc69da
NemiahHayes <104559735+NemiahHayes@users.noreply.github.com>
2024-09-29 19:35:03 +1300

    IniParser and git ignore

--- GPFramework/source/GP Framework/iniparser.cpp
+++ GPFramework/source/GP Framework/iniparser.cpp
@@ -1,64 +1,189 @@
+//Local
 #include "iniparser.h"
-
 #include "logmanager.h"
 
-#include <fstream>
+//Library
 #include <string>
+#include <fstream>
+#include <map>
 
-using std::string;
 
+using namespace std;
+
+IniParser* IniParser::m_pInstance = 0;
+
+IniParser::IniParser()
+	: m_pIniData(new std::map<string, std::map<string, string>>())
+{
+
+}
+
+IniParser::~IniParser()
+{
+	delete m_pIniData;
+	m_pIniData = 0;
+}
+
+IniParser&
+IniParser::GetInstance()
+{
+	if (m_pInstance == 0)
+	{
+		m_pInstance = new IniParser();
+	}
+
+	return (*m_pInstance);
+}
+
+void
+IniParser::DestroyInstance()
+{
+	delete m_pInstance;
+	m_pInstance = nullptr;
+}
 
 bool
 IniParser::LoadIniFile(const string& filename)
 {
-	std::fstream file;
-	string line;
-	file.open(filename);
-	if (file.is_open())
-	{
-		string currentSection;
-		while (std::getline(file, line))
-		{
-			if (!line.empty())
-			{
-				if (line.front() == '[')
-				{
-					currentSection = line;
-					currentSection.erase(0, 1);
-					currentSection.erase(currentSection.find_last_of(']'));
-				}
-				else
-				{
-					int length = line.find_first_of('=');
+	ifstream fileData;
+	std::map<string, std::map<string, string>> iniMap;
 
-					m_dataMap.insert({ currentSection + "|" + line.substr(0, length - 1), line.substr(length + 2, sizeof(line)) });
-				}
-				if (file.fail())
-				{
-					break;
-				}
-			}
-			
-		}
-	}
-	else
+	fileData.open(filename);
+	if (!fileData.is_open())
 	{
 		return false;
 	}
 
-	for (const auto& pair : m_dataMap)
+	string line;
+	string currentSection;
+	while (std::getline(fileData, line))
 	{
-		string output = pair.first.c_str();
-		output.append("\n");
+		line = Trim(line);
 
-		output.append(pair.second.c_str());
-		output.append("\n");
-		LogManager::GetInstance().Log(output.c_str());
+		//Ignore Empty Lines
+		if (line.empty() || line[0] == ';' || line[0] == '#')
+		{
+			continue;
+		}
+
+		//Section Handler
+		if (line.front() == '[' && line.back() == ']')
+		{
+			currentSection = line.substr(1, line.size() - 2);
+		}
+		else
+		{
+			size_t pos = line.find('=');
+			if (pos != string::npos)
+			{
+				string key = Trim(line.substr(0, pos));
+				string value = Trim(line.substr(pos + 1));
+				(*m_pIniData)[currentSection][key] = value;
+			}
+		}
 	}
-	LogManager::GetInstance().Log("Getting a value:");
-	string key = "sectionName|itemName";
-	auto it = m_dataMap.find(key);
-	LogManager::GetInstance().Log(it->second.c_str());
 
+	fileData.close();
 	return true;
+}
+
+string
+IniParser::Trim(const string& str)
+{
+	size_t first = str.find_first_not_of(" \t\n\r\f\v");
+	size_t last = str.find_last_not_of(" \t\n\r\f\v");
+
+	return (first == std::string::npos || last == std::string::npos)
+		? "" : str.substr(first, last - first + 1);
+}
+
+
+string
+IniParser::GetValueAsString(const string& iniSection, const string& key)
+{
+	auto iteratorSection = m_pIniData->find(iniSection);
+	if (iteratorSection != m_pIniData->end())
+	{
+		auto iteratorKey = iteratorSection->second.find(key);
+		if (iteratorKey != iteratorSection->second.end())
+		{
+			return iteratorKey->second;
+		}
+	}
+	return "";
+}
+
+void
+IniParser::PrintMapValues()
+{
+	for (const auto& section : *m_pIniData)
+	{
+		string logString = "[" + section.first + "]";
+		LogManager::GetInstance().Log(logString.c_str());
+
+		for (const auto& keyValue : section.second)
+		{
+			string logString = keyValue.first + " = " + keyValue.second;
+			LogManager::GetInstance().Log(logString.c_str());
+		}
+	}
+}
+
+int
+IniParser::GetValueAsInt(const string& iniSection, const string& key)
+{
+	string data = GetValueAsString(iniSection, key);
+	try
+	{
+		return std::stoi(data);
+	}
+	catch (const std::invalid_argument)
+	{
+		string logString = "Invalid argument for int conversion: " + data;
+		LogManager::GetInstance().Log(logString.c_str());
+		return 0;
+	}
+	catch (const std::out_of_range&)
+	{
+		string logString = "Out of range for int conversion: " + data;
+		return 0;
+	}
+}
+
+float
+IniParser::GetValueAsFloat(const string& iniSection, const string& key)
+{
+	std::string data = GetValueAsString(iniSection, key);
+	try
+	{
+		float value = std::stof(data);
+		return value;
+	}
+	catch (const std::invalid_argument&)
+	{
+		string logString = "Invalid argument for float conversion: " + data;
+		LogManager::GetInstance().Log(logString.c_str());
+		return 0.0f;
+	}
+	catch (const std::out_of_range&)
+	{
+		string logString = "Out of range for float conversion: " + data;
+		LogManager::GetInstance().Log(logString.c_str());
+		return 0.0f;
+	}
+}
+
+bool
+IniParser::GetValueAsBoolean(const string& iniSection, const string& key)
+{
+	int data = GetValueAsInt(iniSection, key);
+
+	if (data == 0)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
 }
