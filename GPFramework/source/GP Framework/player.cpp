1b36e38555728eb42865248a9c5b487b1ec34b14
PatrickEar <uni.patrickear@gmail.com>
2024-10-07 18:13:01 +1300

    feat: Integrated Player Animations
    Still need to figure logic for jumping and falling

--- GPFramework/source/GP Framework/player.cpp
+++ GPFramework/source/GP Framework/player.cpp
@@ -6,7 +6,7 @@
 #include "inputsystem.h"
 
 Player::Player()
-	: m_bSwitchingAnimation(false)
+	: m_bSwitchingAnimation(false), m_bJumping(false), m_bFalling(false)
 {
 }
 
@@ -21,13 +21,13 @@ bool Player::Initialise(Renderer& renderer)
 	m_iInputSystem = new InputSystem();
 	m_iInputSystem->Initialise();
 
-	m_pASprite = renderer.CreateAnimatedSprite("sprites\\anim8strip.png");
-	m_pASprite->SetupFrames(64, 64);
-	m_pASprite->SetFrameDuration(1.0f);
-	m_pASprite->SetScale(1.0f);
+	m_pASprite = renderer.CreateAnimatedSprite("sprites\\playersheet.png");
+	m_pASprite->SetupFrames(48, 48);
+	m_pASprite->SetFrameDuration(0.2f);
+	m_pASprite->SetScale(4.0f);
 	m_pASprite->SetLooping(false);
 
-	AnimationCount0to3();
+	AnimationRunning();
 
 	m_pASprite->Animate();
 	m_pASprite->SetLooping(true);
@@ -59,9 +59,16 @@ void Player::Process(float deltaTime)
 			m_iActiveSpell = ICE;
 		}
 
+		/*
+		if (IsKeySpace() && !m_bSwitchingAnimation)
+		{
+
+		}
+		*/
+
 		if (!m_pASprite->IsAnimating() && m_bSwitchingAnimation)
 		{
-			AnimationCount0to3();
+			AnimationRunning();
 			m_bSwitchingAnimation = false;
 		}
 	}
@@ -90,52 +97,87 @@ bool Player::IsKey3()
 	return (m_iInputSystem->GetKeyState(SDL_SCANCODE_3) == BS_PRESSED);
 }
 
-void Player::AnimationCount0to3()
+/*
+bool Player::IsKeySpace()
+{
+	return (m_iInputSystem->GetKeyState(SDL_SCANCODE_SPACE) == BS_PRESSED);
+}
+*/
+
+void Player::AnimationRunning()
 {
 	m_pASprite->SetLooping(true);
 	m_pASprite->Animate();
-	m_pASprite->SetAnimationRange(0, 3);
+	m_pASprite->SetAnimationRange(0, 7);
 	m_pASprite->Restart();
 }
 
-void Player::AnimationCount4to5()
+/*
+void Player::AnimationJump()
 {
-	m_pASprite->SetAnimationRange(4, 5);
+	m_pASprite->SetLooping(true);
+	m_pASprite->SetAnimationRange(20, 21);
+}
+
+void Player::AnimationFall()
+{
+	m_pASprite->SetLooping(true);
+	m_pASprite->SetAnimationRange(22, 23);
+}
+*/
+
+void Player::AnimationFire()
+{
+	m_pASprite->SetAnimationRange(8, 11);
 	m_pASprite->Restart();
 }
 
-void Player::AnimationCount5to6()
+void Player::AnimationEarth()
 {
-	m_pASprite->SetAnimationRange(5, 6);
+	m_pASprite->SetAnimationRange(12, 15);
 	m_pASprite->Restart();
 }
 
-void Player::AnimationCount6to7()
+void Player::AnimationIce()
 {
-	m_pASprite->SetAnimationRange(6, 7);
+	m_pASprite->SetAnimationRange(16, 19);
 	m_pASprite->Restart();
 }
 
 void Player::AnimateAnimationFire()
 {
 	m_pASprite->SetLooping(false);
-	AnimationCount4to5();
+	AnimationFire();
 	m_bSwitchingAnimation = true;
 }
 
 void Player::AnimateAnimationEarth()
 {
 	m_pASprite->SetLooping(false);
-	AnimationCount5to6();
+	AnimationEarth();
 	m_bSwitchingAnimation = true;
 }
 
 void Player::AnimateAnimationIce()
 {
 	m_pASprite->SetLooping(false);
-	AnimationCount6to7();
+	AnimationIce();
 	m_bSwitchingAnimation = true;
 }
+
+/*
+void Player::AnimateAnimationJump()
+{
+	AnimationJump();
+	m_bSwitchingAnimation = true;
+}
+
+void Player::AnimateAnimationFall()
+{
+	AnimationFall();
+	m_bSwitchingAnimation = true;
+}
+*/
 
 ElementType Player::GetActiveSpell() const
 {
